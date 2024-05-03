766d4a36c4f1b0dc7ed4f9a900ddde5d3dc7d0ca
Michael Krtička <krticka.michael@gmail.com>
2024-05-03 22:38:17 +0200

    Java akademie 1 lekce 1 cvičení

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+package com.engeto.invoice;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String item = "trekové boty sedmimílovky";
+        int quantity = 3;
+        double weight = 0.45;
+        int price = 1300;
+        LocalDate dateofissue = LocalDate.of(2022,4,7);
+        boolean isDiscounted = true;
+
+        double totalWeight = (quantity * weight);
+        System.out.println("Celková váha je " +totalWeight +"Kg");
+
+        int totalPrice = (quantity * price);
+        System.out.println("Celková cena je " +totalPrice +"Kč");
+
+        int shippingPrice = 45;
+        System.out.println("Celková cena po započtení dopravy je " +(totalPrice + shippingPrice) +"Kč");
+
+        int Discount = (totalPrice/100)*5;
+        System.out.println("Velikost slevy "+Discount);
+        System.out.println("Celková cena po započtení slevy 5% je " +(totalPrice - Discount + shippingPrice) +"Kč");
+    }
+}
